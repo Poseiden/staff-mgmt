ff92a18ae71b880ce36013d7b91871058854066c
hbzhu <hbzhu@thoughtworks.com>
2022-03-11 16:15:19 +0800

    [feat] add dbo.

---
+++ src/main/java/staff/mgmt/infrastructure/persistence/db/table/SkillCategoryDBO.java
@@ -0,0 +1,31 @@
+package staff.mgmt.infrastructure.persistence.db.table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.Generated;
+import org.hibernate.annotations.GenerationTime;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table
+public class SkillCategoryDBO {
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "org.hibernate.id.UUIDGenerator")
+    @Generated(GenerationTime.INSERT)
+    private String id;
+    private String name;
+}
